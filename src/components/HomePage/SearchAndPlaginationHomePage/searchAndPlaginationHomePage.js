7a0eced1b11bd20bd5d83079201c42009724b3e6
vitkhom <vitalii.s.khomenko@gmail.com>
2020-08-09 18:11:11 +0300

    Commit 2

--- src/components/HomePage/SearchAndPlaginationHomePage/searchAndPlaginationHomePage.js
+++ src/components/HomePage/SearchAndPlaginationHomePage/searchAndPlaginationHomePage.js
@@ -1,118 +1,95 @@
 'use strict';
-
 import api from '../../../services/movies-api-service';
 import spinner from '../../../utils/spinner';
 import notify from '../../../utils/notify';
 
 import searchPage from './searchAndPlaginationHomePage.hbs';
-import methods from '../InitialHomePage/initialHomePage';
+import initialHomePage from '../InitialHomePage/initialHomePage';
 import './searchAndPlaginationHomePage.scss';
-
 import icon from '../../../images/icon.png';
 
+const debounce = require('lodash.debounce');
 let inputValue = '';
+let totalPages = null;
 
-export default function (root, ...rest) {
+export default function (root) {
   const markup = searchPage({ icon });
   root.insertAdjacentHTML('beforeend', markup);
-  methods.fetchPopularMoviesList();
-
-  const debounce = require('lodash.debounce');
+  initialHomePage.fetchPopularMoviesList();
 
   const refs = {
     input: document.querySelector('.js-searchInput'),
+    form: document.querySelector('.serach-form'),
     gallery: document.querySelector('#js-film-gallery'),
     prevBtn: document.querySelector('#prevBtn'),
     nextBtn: document.querySelector('#nextBtn'),
     pageNumber: document.querySelector('#paginationPageNumber'),
+    paginationWrapper: document.querySelector('.paginationWrapper'),
   };
 
-  refs.input.addEventListener('input', debounce(searchForm, 500));
-  // refs.input.addEventListener('keypress', searchForm);
+  refs.input.addEventListener('input', debounce(searchForm, 1500));
   refs.prevBtn.addEventListener('click', prevPageHandler);
   refs.nextBtn.addEventListener('click', nextPageHandler);
 
   function searchForm(e) {
     e.preventDefault();
-
-    methods.resetPage();
-    refs.pageNumber.textContent = methods.pageNumber;
-
     inputValue = e.target.value;
-    const page = methods.pageNumber;
-
-    clearMovies();
-
-    spinner.show();
-
-    inputValue
-      ? api
-          .fetchShowWithQuery(inputValue, page)
-          .then(movies =>
-            movies.length > 0
-              ? methods.createCardFunc(movies)
-              : notify.showError(),
-          )
-          .catch(error => {
-            console.log(error);
-            notify.showError();
-          })
-          .finally(() => spinner.hide())
-      : methods.fetchPopularMoviesList();
-
+    initialHomePage.resetPage();
+    resetPageNumber();
+    clearGallery();
+    selectFetch(inputValue);
     e.target.value = '';
   }
 
   function nextPageHandler() {
-    methods.incrementPage();
-    refs.pageNumber.textContent = methods.pageNumber;
-
-    const query = inputValue;
-    const page = methods.pageNumber;
-
-    clearMovies();
-
-    query
-      ? (spinner.show(),
-        api
-          .fetchShowWithQuery(query, page)
-          .then(movies => methods.createCardFunc(movies))
-          .catch(error => {
-            console.log(error);
-            notify.showError();
-          })
-          .finally(() => spinner.hide()))
-      : methods.fetchPopularMoviesList();
+    if (totalPages <= initialHomePage.pageNumber) {
+      notify.showNoMatches();
+      return;
+    }
+    initialHomePage.incrementPage();
+    resetPageNumber();
+    clearGallery();
+    selectFetch(inputValue);
   }
 
   function prevPageHandler() {
-    if (methods.pageNumber <= 1) {
+    if (initialHomePage.pageNumber <= 1) {
       return;
     }
-
-    methods.decrementPage();
-    refs.pageNumber.textContent = methods.pageNumber;
-
-    const query = inputValue;
-    const page = methods.pageNumber;
-
-    clearMovies();
-
-    query
-      ? (spinner.show(),
-        api
-          .fetchShowWithQuery(query, page)
-          .then(movies => methods.createCardFunc(movies))
-          .catch(error => {
-            console.log(error);
-            notify.showError();
-          })
-          .finally(() => spinner.hide()))
-      : methods.fetchPopularMoviesList();
+    initialHomePage.decrementPage();
+    resetPageNumber();
+    clearGallery();
+    selectFetch(inputValue);
   }
 
-  function clearMovies() {
+  function selectFetch(query) {
+    query ? fetchMovies(query) : initialHomePage.fetchPopularMoviesList();
+  }
+
+  function fetchMovies(query) {
+    const { pageNumber } = initialHomePage;
+    spinner.show();
+    api
+      .fetchShowWithQuery(query, pageNumber)
+      .then(({ results, total_pages }) => {
+        totalPages = total_pages;
+        totalPages > 0
+          ? (refs.paginationWrapper.classList.remove('invisible'),
+            initialHomePage.createCardFunc(results),
+            initialHomePage.createRenderFilmsArray(results))
+          : (notify.showNoMatches(),
+            refs.paginationWrapper.classList.add('invisible'));
+      })
+      .catch(error => notify.showError())
+      .finally(() => spinner.hide());
+  }
+
+  function resetPageNumber() {
+    const { pageNumber } = initialHomePage;
+    refs.pageNumber.textContent = pageNumber;
+  }
+
+  function clearGallery() {
     refs.gallery.innerHTML = '';
-    methods.renderFilms = [];
   }
 }
