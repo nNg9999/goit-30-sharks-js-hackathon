1008026966885e48f462bec0387ff7bf8b1c28e7
Aleksandra Tkachuk <lesyalesya9876@gmail.com>
2020-08-11 15:46:02 +0300

    added initialHomePage

---
+++ src/components/HomePage/InitialHomePage/initialHomePage.js
@@ -0,0 +1,97 @@
+import spinner from '../../../utils/spinner';
+import notify from '../../../utils/notify';
+import apiService from '../../../services/movies-api-service';
+import navigation from '../../Header/navigation';
+import initialHomePageTemplate from './initialHomePage.hbs';
+import './initialHomePage.scss';
+
+export default {
+  renderFilms: [],
+  pageNumber: 1,
+  totalPages: 0,
+
+  incrementPage() {
+    this.pageNumber += 1;
+  },
+
+  decrementPage() {
+    this.pageNumber -= 1;
+  },
+
+  resetPage() {
+    this.pageNumber = 1;
+  },
+
+  makeInvisible() {
+    if (!pagination.classList.contains('invisible')) {
+      pagination.classList.add('invisible');
+    }
+  },
+
+  fetchPopularMoviesList() {
+    spinner.show();
+    apiService
+      .fetchShowWithTrending(this.pageNumber)
+      .then(({ results, total_pages }) => {
+        this.checkConditionsForRender(results, total_pages);
+      })
+      .catch(error => {
+        console.log(error);
+        notify.showError();
+      })
+      .finally(() => spinner.hide());
+  },
+
+  checkConditionsForRender(films, numberOfPages) {
+    const pagination = document.querySelector('#pagination');
+    this.totalPages = numberOfPages;
+
+    if (this.totalPages === 0) {
+      notify.showNoMatches();
+      return;
+    }
+
+    if (this.totalPages >= this.pageNumber) {
+      pagination.classList.remove('invisible');
+    }
+
+    this.createRenderFilmsArray(films);
+    this.createCardFunc(films);
+  },
+
+  createRenderFilmsArray(films) {
+    this.renderFilms = [...films];
+  },
+
+  createCardFunc(films) {
+    const markup = films
+      .map(film =>
+        initialHomePageTemplate(this.createFilmObjectForTemplate(film)),
+      )
+      .join(' ');
+    const gallery = document.querySelector('#js-film-gallery');
+    gallery.insertAdjacentHTML('beforeend', markup);
+    gallery.addEventListener('click', this.clickHandler);
+  },
+
+  createFilmObjectForTemplate(film) {
+    const pathToPoster = film.backdrop_path
+      ? `${apiService.pathImage}${film.backdrop_path}`
+      : '../../../images/noPoster.jpg';
+    const title = film.title ? film.title : film.name;
+    const id = film.id;
+    const year = film.release_date ? `(${film.release_date.slice(0, 4)})` : '';
+    return {
+      pathToPoster,
+      title,
+      id,
+      year,
+    };
+  },
+
+  clickHandler(e) {
+    const clickedFilm = e.target;
+    const clickedFilmId = clickedFilm.dataset.id;
+    navigation.activeDetailsPage(clickedFilmId, false);
+  },
+};
