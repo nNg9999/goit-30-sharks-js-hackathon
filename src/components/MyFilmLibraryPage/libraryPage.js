3d891eb796a1b84829ddbda70e38660a600530e7
nNg9999 <n_n_g_@ukr.net>
2020-08-11 15:31:58 +0300

    mpdif 5myLibrary

--- src/components/MyFilmLibraryPage/libraryPage.js
+++ src/components/MyFilmLibraryPage/libraryPage.js
@@ -1,93 +1,248 @@
-import newfetchShowDetails from '../../services/movies-api-service';
-
 import myFilmLibraryPageTemplate from './myFilmLibraryPage.hbs';
 import filmLibraryItemsTemplate from './filmLibraryItems.hbs';
-import debounce from 'lodash.debounce';
 
-import notify from '../../utils/notify';
-import spinner from '../../utils/spinner';
+import storage from '../../utils/storage';
+import './myFilmLibraryPage.scss';
+import navigation from '../Header/navigation';
 
 export default function (root, ...rest) {
-  // Создаем всю разметку 
-
-  const myConstant = 5;
-
-  // const markup = myFilmLibraryPageTemplate();
-  // root.insertAdjacentHTML('beforeend', markup);
-
-  function createLibraryCard(imgPath, filmTitle, movieId, voteAverage) {
-
-    const markup = filmLibraryItems();
-
-    return root.insertAdjacentHTML('beforeend', markup);
-  }
-
-  // createLibraryCard();
-
-
-  function drawQueueFilmList(params) {
-
-  }
-
-  function drawWatchedFilmList(params) {
-
-  }
-
-
   const markup = myFilmLibraryPageTemplate();
   root.insertAdjacentHTML('beforeend', markup);
 
-
   const refs = {
     userInput: document.querySelector('#name-input'),
-    moviesList: document.querySelector('#movies-list'),
-    watched: document.querySelector('watched'),
-    queue: document.querySelector('queue'),
+    moviesListWatched: document.querySelector('#js-watched'),
+    moviesListQueue: document.querySelector('#js-queue'),
+    watched: document.querySelector('.watched'),
+    queue: document.querySelector('.queue'),
+    moviesListItem: document.querySelector('.movie-list__item'),
   };
 
-  refs.userInput.addEventListener('input', debounce(searchFormHandler, 500));
+  // refs.moviesListItem.addEventListener('click', navigation.activeDetailsPage);
+  function createLibraryCardFunc(fragment = '') {
+    return filmLibraryItemsTemplate(fragment);
+  }
 
-  function searchFormHandler(e) {
-    clearListItems();
+  function drawQueueFilmList() {
+    let fragments = '';
+    fragments = storage.get('filmsQueue');
 
-    const searchQuery = refs.userInput.value || '';
-
-    if (searchQuery) {
-      fetchMovies(searchQuery);
+    if (fragments !== null && fragments.length > 0) {
+      fragments.map(element => {
+        const markup = createLibraryCardFunc(element);
+        refs.moviesListQueue.insertAdjacentHTML('beforeend', markup);
+      });
+    } else {
+      const markup = createPlug('queue');
+      refs.moviesListQueue.insertAdjacentHTML('beforeend', markup);
     }
-
   }
 
-  function fetchMovies(searchQuery) {
-    spinner.show();
-    newfetchShowDetails.fetchShowWithQuery(searchQuery)
-      .then(data => {
-        console.log(data);
-        renderListItems(data);
-      })
-      .catch(notify.showError('ERROR!'));
+  function drawWatchedFilmList() {
+    let fragments = '';
+    fragments = storage.get('filmsWatched');
+
+    if (fragments !== null && fragments.length > 0) {
+      fragments.forEach(element => {
+        const markup = createLibraryCardFunc(element);
+        refs.moviesListWatched.insertAdjacentHTML('beforeend', markup);
+      });
+    } else {
+      const markup = createPlug('watched');
+      refs.moviesListWatched.insertAdjacentHTML('beforeend', markup);
+    }
   }
 
-  function renderListItems(items) {
-    spinner.hide();
-
-    let markup = '';
-
-    markup = filmLibraryItemsTemplate(items);
-    notify.showInfo('привет!!');
-
-    refs.moviesList.insertAdjacentHTML('beforeend', markup);
+  function createPlug(text) {
+    return `<div>
+      You do not have to ${text} queue movies to watch.Add them.
+    </div>`;
   }
 
+  function clearListItems() {
+    refs.moviesListWatched.innerHTML = '';
+    refs.moviesListQueue.innerHTML = '';
+  }
 
+  function showWatched() {
+    refs.watched.classList.add('active');
+    refs.queue.classList.remove('active');
+  }
 
+  function hidenWatched() {
+    refs.watched.classList.remove('active');
+    refs.queue.classList.add('active');
+  }
 
-
-  refs.userInput.addEventListener('click', handlerClickWatched);
+  refs.watched.addEventListener('click', handlerClickWatched);
+  refs.queue.addEventListener('click', handlerClickQueue);
 
   function handlerClickWatched(e) {
+    e.preventDefault();
+    const movieId = e.target.getAttribute('id');
+    // location.pathname = '/details';
+    // navigation.activeDetailsPage(movieId, true);
     clearListItems();
-    console.log('handlerClickWatched');
+    drawWatchedFilmList();
+    navigation.activeDetailsPage(movieId, true);
+    showWatched();
   }
 
+  function handlerClickQueue(e) {
+    e.preventDefault();
+    const movieId = e.target.getAttribute('id');
+    // navigation.activeDetailsPage(movieId, true);
+    clearListItems();
+    drawQueueFilmList();
+    // navigation.activeDetailsPage(movieId, true);
+    hidenWatched();
+  }
+  drawWatchedFilmList();
+  showWatched();
 }
+
+
+
+
+// import myFilmLibraryPageTemplate from './myFilmLibraryPage.hbs';
+// import filmLibraryItemsTemplate from './filmLibraryItems.hbs';
+
+
+// import storage from '../../utils/storage';
+// import './myFilmLibraryPage.scss';
+
+// export default function (root, ...rest) {
+//   // Создаем всю разметку 
+//   // const db = [
+//   //   {
+//   //     backdrop_path: "/sEku5PGhCZFvVxyuIG7PYPAsloc.jpg",
+//   //     genre_ids: [27],
+//   //     movieId: 24655,
+//   //     original_title: "The Car",
+//   //     poster_path: "/gFM9yMrO4fQMiG440Lx6bazB517.jpg",
+//   //     release_date: "1977-05-12",
+//   //     title: "The Car",
+//   //     vote_average: 6,
+//   //   },
+//   //   {
+//   //     backdrop_path: "/sEku5PGhCZFvVxyuIG7PYPAsloc.jpg",
+//   //     genre_ids: [27],
+//   //     movieId: 24655,
+//   //     original_title: "The Car",
+//   //     poster_path: "/gFM9yMrO4fQMiG440Lx6bazB517.jpg",
+//   //     release_date: "1977-05-12",
+//   //     title: "The Car",
+//   //     vote_average: 6,
+//   //   },
+
+//   // ];
+
+//   const db = [
+//     { id: 475557, title: "Джокер", vote: 8.2, poster: "/498AdCguRoD1mpizIeKi8nDTOQO.jpg" },
+//     { id: 475557, title: "Джокер", vote: 8.2, poster: "/498AdCguRoD1mpizIeKi8nDTOQO.jpg" },
+//     { id: 475557, title: "Джокер", vote: 8.2, poster: "/498AdCguRoD1mpizIeKi8nDTOQO.jpg" },
+//   ]
+
+//   storage.save('filmsQueue', db);
+//   storage.save('filmsWatched', db);
+
+
+//   const markup = myFilmLibraryPageTemplate();
+//   root.insertAdjacentHTML('beforeend', markup);
+
+//   const refs = {
+//     userInput: document.querySelector('#name-input'),
+//     moviesListWatched: document.querySelector('#js-watched'),
+//     moviesListQueue: document.querySelector('#js-queue'),
+//     watched: document.querySelector('.watched'),
+//     queue: document.querySelector('.queue'),
+//   };
+
+//   function createLibraryCardFunc(fragment = "") {
+//     return filmLibraryItemsTemplate(fragment);
+//   }
+
+
+
+//   function drawQueueFilmList() {
+//     let fragments = "";
+//     fragments = storage.get("filmsQueue");
+
+//     if (fragments !== null && fragments.length > 0) {
+
+//       fragments.map(element => {
+//         const markup = createLibraryCardFunc(element);
+//         refs.moviesListQueue.insertAdjacentHTML('beforeend', markup);
+//       });
+
+//     }
+//     else {
+//       const markup = createPlug("queue");
+//       refs.moviesListQueue.insertAdjacentHTML('beforeend', markup);
+//     }
+//   }
+
+//   function drawWatchedFilmList() {
+
+//     let fragments = "";
+//     fragments = storage.get("filmsWatched");
+
+//     if (fragments !== null && fragments.length > 0) {
+
+//       fragments.forEach(element => {
+//         const markup = createLibraryCardFunc(element);
+//         refs.moviesListWatched.insertAdjacentHTML('beforeend', markup);
+//       });
+
+//     }
+//     else {
+//       const markup = createPlug("watched");
+//       refs.moviesListWatched.insertAdjacentHTML('beforeend', markup);
+//     }
+//   }
+
+//   function createPlug(text) {
+//     return `<div>
+//       You do not have to ${text} queue movies to watch.Add them.
+//     </div>`
+//   }
+
+//   function clearListItems() {
+//     refs.moviesListWatched.innerHTML = '';
+//     refs.moviesListQueue.innerHTML = '';
+//   }
+
+
+//   function showWatched() {
+//     refs.watched.classList.add('active');
+//     refs.queue.classList.remove('active');
+//   }
+
+//   function hidenWatched() {
+//     refs.watched.classList.remove('active');
+//     refs.queue.classList.add('active');
+//   }
+
+//   refs.watched.addEventListener('click', handlerClickWatched);
+//   refs.queue.addEventListener('click', handlerClickQueue);
+
+//   function handlerClickWatched(e) {
+//     e.preventDefault();
+//     const movieId = e.target.getAttribute("id");
+//     // activeDetailsPage(movieId, true);
+//     clearListItems();
+//     drawWatchedFilmList();
+//     showWatched();
+//   }
+
+//   function handlerClickQueue(e) {
+//     e.preventDefault();
+//     const movieId = e.target.getAttribute("id");
+//     // activeDetailsPage(movieId, true);
+//     clearListItems();
+//     drawQueueFilmList();
+//     hidenWatched();
+//   }
+// }
+
+
--- src/pages/DetailsPage/DetailsPage.js
+++ src/pages/DetailsPage/DetailsPage.js
@@ -2,16 +2,16 @@ import DetailsPage from '../../components/DetailsPage/filmsDetailsPage';
 
 export default function (root) {
     DetailsPage(root);
-    eventsHandlers(root);
+    // eventsHandlers(root);
 }
 
-function eventsHandlers(root) {
-    const goHome = document.querySelector('.go-home');
+// function eventsHandlers(root) {
+//     const goHome = document.querySelector('.go-home');
 
-    goHome.addEventListener('click', () => {
-        root.innerHTML = '';
-        DetailsPage(root);
-        location.pathname = '/';
-    })
-}
+//     goHome.addEventListener('click', () => {
+//         root.innerHTML = '';
+//         DetailsPage(root);
+//         location.pathname = '/';
+//     })
+// }
 
